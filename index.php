0f011a5a2fcac129f7aee2220bd066a636ae9144
Mahdi <mahdi.ndc21@gmail.com>
2024-08-23 18:47:48 +0600

    sign-in front end

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/signin.css">
+    <title>Bookworm Buddies</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="form-container">
+            <h1 class="logo">Bookworm Buddies</h1>
+            <form action="signin.php" method="post">
+                <label for="username">Username:</label>
+                <br>
+                <input class="textbox" type="text" name="username">
+                <br>
+                <label for="password">Password:</label>
+                <br>
+                <input class="textbox" type="password" name="password">
+                <br>
+                <button class="button"><strong>Sign In</strong></button>
+                <br>
+                <a class="anchor" href="signup.php">Don't have an account? Sign up here!</a>
+            </form>
+        </div>
+    </div>
+    <br>
+    
+    
+   
+</body>
+</html>
