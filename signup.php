8ec1ba3dfee198670c3ed984ff993a7e6b19884b
B M Rauf <bmrauf@outlook.com>
2024-08-24 13:56:21 +0600

    added php and mysql query on signup page

--- signup.php
+++ signup.php
@@ -10,6 +10,46 @@
 <div class="container">
         <div class="form-container">
             <?php
+            include("dbconnect.php");
+
+            if(isset($_POST['submit'])){
+                $fname = $_POST['fname'];
+                $lname = $_POST['lname'];
+                $dob = $_POST['dob'];
+                $email = $_POST['email'];
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+
+                $verify_email = mysqli_query($conn,"SELECT email FROM users WHERE email='$email'");
+                $verify_username = mysqli_query($conn,"SELECT username FROM users WHERE username='$username'");
+                
+                if(mysqli_num_rows($verify_email) != 0) {
+                    echo "<div class='message'>
+                        <p>Email already used. Please, try again.</p>
+                        </div> <br>";
+                } elseif (mysqli_num_rows($verify_username) != 0) {
+                    echo "<div class='message'>
+                        <p>Username already used. Please, try again.</p>
+                        </div> <br>";
+                } else{
+                    // Hash the password for security before storing
+                    // $hashed_password = password_hash($password, PASSWORD_BCRYPT); we can use hashed password for security
+                    
+                    // Insert the user data into the database
+                    $query = "INSERT INTO users(username, password, email, first_name, last_name, date_of_birth, points) VALUES('$username', '$password', '$email', '$fname', '$lname', '$dob', 0)";
+                    if (mysqli_query($conn, $query)) {
+                        echo "<div class='message'>
+                            <p>Registration successful!</p>
+                            </div> <br>";
+                        echo "<a href='index.php'><button class='button'>Login Now</button></a>"; 
+                    } else {
+                        echo "<div class='message'>
+                            <p>Error: " . mysqli_error($conn) . "</p>
+                            </div> <br>";
+                        echo "<a href='javascript:self.history.back()'><button class='button'>Go Back</button></a>";
+                    }
+                }
+            }
             ?>
             <h1 class="logo">Bookworm Buddies</h1>
             <form action="signup.php" method="post">
@@ -37,11 +77,10 @@
                 <br>
                 <input class="textbox" type="password" name="password" required>
                 <br>
-                <button class="button"><strong>Sign Up</strong></button>
+                <button class="button" name="submit" type="submit"><strong>Sign Up</strong></button>
                 <br>
             </form>
         </div>
     </div>
-       
 </body>
 </html>
