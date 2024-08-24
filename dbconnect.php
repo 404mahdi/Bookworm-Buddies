39122fa2740b76461e25bd788241faa5ea3ce36a
B M Rauf <bmrauf@outlook.com>
2024-08-24 22:27:02 +0600

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -10,6 +10,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Connected successfully";
 ?>
