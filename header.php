8f709b03e7e0e4541ab40dcc3f26a23b4bdbb140
Mahdi <mahdi.ndc21@gmail.com>
2024-08-30 12:44:36 +0600

    header

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Responsive Navbar</title>
+    <link rel="stylesheet" href="./css/header.css" />
+  </head>
+  <body>
+    <nav class="navbar">
+      <div class="logo">Bookworm Buddies</div>
+
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+        <button class="search-btn">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+        
+      </div>
+      <ul class="nav-links">
+        <li><a href="./home.php">Home</a></li>
+        <li><a href="#about">All Books</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">My profile</a></li>
+      </ul>
+    </nav>
+
+    <script
+      src="https://kit.fontawesome.com/4599b1e468.js"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
