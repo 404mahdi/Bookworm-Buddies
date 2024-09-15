afea9a275ab5b1b78a3d10b8aa22dd134f06465f
B M Rauf <bmrauf@outlook.com>
2024-09-15 20:33:41 +0600

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Responsive Navbar</title>
+    <title>Bookworm Buddies</title>
     <link rel="stylesheet" href="./css/header.css" />
   </head>
   <body>
